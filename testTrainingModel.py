5857cfaeff8d9883c59f618d8870f98ba7ac7834
allesman <67746449+allesman@users.noreply.github.com>
2024-08-27 17:45:08 +0200

    change some default values to hopefully get results quicker

--- testTrainingModel.py
+++ testTrainingModel.py
@@ -8,16 +8,16 @@ class TrainingConfig:
     image_size = 128  # the generated image resolution
     train_batch_size = 16
     eval_batch_size = 16  # how many images to sample during evaluation
-    num_epochs = 50
+    num_epochs = 1
     gradient_accumulation_steps = 1
     learning_rate = 1e-4
     lr_warmup_steps = 500
-    save_image_epochs = 10
+    save_image_epochs = 1
     save_model_epochs = 30
     mixed_precision = "fp16"  # `no` for float32, `fp16` for automatic mixed precision
     output_dir = "ddpm-butterflies-128"  # the model name locally and on the HF Hub
 
-    push_to_hub = True  # whether to upload the saved model to the HF Hub
+    push_to_hub = False  # whether to upload the saved model to the HF Hub
     hub_model_id = "<your-username>/<my-awesome-model>"  # the name of the repository to create on the HF Hub
     hub_private_repo = False
     overwrite_output_dir = True  # overwrite the old model when re-running the notebook
@@ -25,7 +25,6 @@ class TrainingConfig:
 
 
 config = TrainingConfig()
-config.push_to_hub = False
 
 from datasets import load_dataset
 
